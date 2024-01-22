c0b784f126b0ae22d63fc615e138be1c2696c9da
张浩 <zhanghao@trinasky.com>
2024-01-22 16:47:24 +0800

    添加404页面 解决router因为index_on_404 = true缺失导致的失效问题

--- src/route/router.rs
+++ src/route/router.rs
@@ -1,10 +1,17 @@
 #![allow(non_snake_case)]
+use crate::views::home::Home;
+use crate::views::page_not_found::PageNotFound;
 use crate::views::signin::SignIn;
 use dioxus::prelude::*;
 pub use dioxus_router::prelude::*;
 
+#[rustfmt::skip]
 #[derive(Routable, Clone)]
 pub enum Route {
-    #[route("/sign-in")]
+    #[route("/")]
+    Home {},
+    #[route("/sign")]
     SignIn {},
+    #[route("/:..route")]
+    PageNotFound { route: Vec<String> },
 }
